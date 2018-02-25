f7e44c59b997dfaec446637c6636acf28aba182d
shaghz <sh.khangaldy@gmail.com>
2018-02-25 17:51:40 +0330

    m

--- index.js
+++ index.js
@@ -1,74 +1,34 @@
 //configurations
-let dateNow = moment().locale('fa').format('YYYY/M/D');
-let dateArray = dateNow.split('/');
-let year = dateArray[0];
-let month = dateArray[1];
-const monthNames = ["", "farvardin", "ordibehesht", "khordad", "tir", "mordad", "shahrivar", "mehr", "aban", "azar", "dey", "bahman", "esfand"];
-let myJSON = '{ "date":"1396/11/27"}';
-$(document).ready(function() {
-    setDate();
-    displayCalendar();
+(function ($) {
+    $.fn.makeChange = function () {
+        moment.locale('fa');
+        let dateNow = moment().locale('fa').format('YYYY/M/D');
+        let m = moment(dateNow);
+        let dateArray = dateNow.split('/');
+        let year = dateArray[0];
+        let month = dateArray[1];
+        const monthNames = ["", "farvardin", "ordibehesht", "khordad", "tir", "mordad", "shahrivar", "mehr", "aban", "azar", "dey", "bahman", "esfand"];
+        const calendarEvents = {
+            "events": [
+                {"caption": "an event", "date": "1396/12/12"},
+                {"caption": "an event2", "date": "1396/12/2"},
+                {"caption": "an event3", "date": "1396/12/12"},
+                {"caption": "an event4", "date": "1396/11/12"}
+            ]
+        };
 
-//set dates
-    function setDate() {
+        displayCalendar();
 
-            moment.locale('fa');
-            m = moment(dateNow);
-            return m;
-    }
+        function displayCalendar() {
 
-    function displayCalendar() {
+            let calendarContent = calendarSetter();
+            //set body
+            document.getElementById("calendar").innerHTML =
 
-        let htmlContent = "";
-
-        // get first day of the months weekday
-        let weekday = moment(dateNow).startOf('jMonth').weekday();
-
-        // get first day of the months weekday
-        let weekday2 = weekday;
-
-        //get month length
-        let numOfDays = moment(dateNow).endOf('month').format("jD");
-        //white space for previous month ,loose one for each cell
-
-        let daysOfMonth =  Array.apply(null, {length: numOfDays}).map(Number.call, Number);
-console.log(daysOfMonth)
-        let daysOfweek = chunks(daysOfMonth,7);
-        console.log(daysOfweek);
-        while (weekday > 0) {
-            htmlContent += "<td class='monthPre'></td>";
-            weekday--;
-        }
-
-        // calander body.
-        let day = 1;
-        while (day <= numOfDays) {
-            // if friday go to new line.
-
-            if (weekday2 > 6) {
-                weekday2 = 0;
-                htmlContent += "</tr><tr>";
-            }
-            //if working with no events
-            // htmlContent += "<td >" + day + "</td>";
-            // handling events.
-            // let event = JSON.parse(myJSON);
-            // let eventDate = (year + '/' + month + '/' + day);
-            //set the cells with attention to events
-            // if (eventDate == event.date) {
-            //     htmlContent += "<td class='event' >" + day + " <div>an eventxry</div></td>";
-            // } else {
-                htmlContent += "<td >" + day + "</td>";
-            // }
-
-            weekday2++;
-            day++;
-        }
-
-        let calendarBody = `<table class='calendar'>
+                `<table class='calendar'>
                             <tr class='monthNow'>
                                 <th colspan='7'>
-                                ${  monthNames[month] } ${year} 
+                                ${  monthNames[month] } ${year}
                                 </th>
                             </tr>
                             <tr class='dayNames'>
@@ -80,31 +40,72 @@ console.log(daysOfMonth)
                                 <td>panjsjanbe</td>
                                 <td>jume</td>
                             </tr>
-                            <tr>
-                                ${htmlContent}
-                            </tr>
+                            
+                                ${calendarContent}
+                       
                         </table>`;
+        }
 
-        document.getElementById("calendar").innerHTML = calendarBody;
-    }
+        function getYearInfo() {
+
+            // get first day of the months weekday
+            let weekStartDay = moment(dateNow).startOf('jMonth').weekday();
+            //get month length
+            let numOfMonthDays = parseInt(moment(dateNow).endOf('month').format("jD"));
+            //white space for previous month ,loose one for each cell
+            let daysOfMonthArray = Array(numOfMonthDays).fill(0).map((e, i) => i + 1);
+
+            while (weekStartDay > 0) {
+                daysOfMonthArray.unshift('');
+                weekStartDay--;
+            }
+            return chunks(daysOfMonthArray, 7);
+        }
+
+//loop to get calendar days and check events
+        function calendarSetter() {
+            let calendarContent = "";
+            let daysOfEachWeek = getYearInfo();
+
+            for (let i = 0; i < daysOfEachWeek.length; i++) {
+                calendarContent += `<tr>`;
+                for (let j = 0; j < daysOfEachWeek[i].length; j++) {
+                    let day = daysOfEachWeek[i][j];
+                    let currentEvent = m.format('YYYY/MM/') + daysOfEachWeek[i][j];
+                    for (let k = 0; k < calendarEvents.events.length; k++) {
+                        if (calendarEvents.events[k].date === currentEvent)
+                            day += `<p class="event">${calendarEvents.events[k].caption}</p>`;
+                    }
+
+                    calendarContent += `<td>
+                                    ${ day } 
+                                 </td>`;
+                }
+                calendarContent += `</tr>`;
+
+            }
+            return calendarContent;
+        }
+
 
 //update view date
-    function updateCalendar() {
-        newDate = moment(dateNow);
-        month = newDate.format('M');
-        year = newDate.format('YYYY');
-        displayCalendar()
-    }
+        function updateCalendar() {
+            month = dateNow.format('M');
+            year = dateNow.format('YYYY');
+            displayCalendar();
+        }
 
 //update on click when next month
-    $("#next").click(function(){
-        dateNow = m.add(1, "month").format("YYYY/MM/DD");
-        updateCalendar();
-    });
+        $("#next").click(function () {
+            dateNow = moment(m.add(1, "month"));
+            updateCalendar();
+        });
 
 //update on click when prev month
-    $("#previous").click(function(){
-        dateNow = m.subtract(1, "month").format("YYYY/MM/DD");
-        updateCalendar();
-    });
-});
+        $("#previous").click(function () {
+            dateNow = moment(m.subtract(1, "month"));
+            updateCalendar();
+        });
+
+    };
+}(jQuery));
